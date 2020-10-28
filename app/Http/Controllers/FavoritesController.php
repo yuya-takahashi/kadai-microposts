e5a60c1f30b5f821739e08cce189f5b169bea21e
yuya.takahashi <Pochaty119@outlook.jp>
2020-10-28 07:29:17 +0000

    navbar Favorites作成

--- app/Http/Controllers/FavoritesController.php
+++ app/Http/Controllers/FavoritesController.php
@@ -8,17 +8,13 @@ class FavoritesController extends Controller
 {
     public function store($id)
     {
-        // 認証済みユーザ（閲覧者）が、 idのユーザをフォローする
         \Auth::user()->favorite($id);
-        // 前のURLへリダイレクトさせる
         return back();
     }
     
     public function destroy($id)
     {
-        // 認証済みユーザ（閲覧者）が、 idのユーザをアンフォローする
         \Auth::user()->unfavorite($id);
-        // 前のURLへリダイレクトさせる
         return back();
     }
 }
